26632f1a112c6ef067f41c07f663ec809ad7641e
sviv <sviv@debian>
2016-05-31 22:02:02 +0300

    small fixes, from list, except accordeon

--- gulpfile.js
+++ gulpfile.js
@@ -71,11 +71,11 @@ gulp.task('js:build', function () {
 
 gulp.task('style:build', function () {
     gulp.src(path.src.style) 
-        .pipe(sourcemaps.init())
+        ///.pipe(sourcemaps.init())
         .pipe(less())
-        .pipe(prefixer())
-        .pipe(cssmin())
-        .pipe(sourcemaps.write())
+        //.pipe(prefixer())
+        //.pipe(cssmin())
+        //.pipe(sourcemaps.write())
         .pipe(gulp.dest(path.build.css))
         .pipe(reload({stream: true}));
 });
